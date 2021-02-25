27ee49c6b7baa6553e076bc9a4cf518c8d7c0b03
PaulB-H <p.bernardhall@gmail.com>
2021-02-25 11:43:52 -0500

    Added connection to socket.io
    Attached socketio to server as io, imported our socket handler, setup our onConnection function & defined there whatever handlers we want to happen, and then setup the route to onConnection

--- server.js
+++ server.js
@@ -10,6 +10,21 @@ const server = app.listen(port, () => {
   console.log(`Listening on port: ${port} \n`);
 });
 
+// Attach socket.io to the server
+const io = require("socket.io")(server);
+
+// Import socket handlers
+const socketConnectHandlers = require("./socket_logic/socketConnect.js");
+
+const onConnection = (socket) => {
+  socketConnectHandlers(io, socket);
+  // We could call more socket handlers here
+};
+
+// Whenever we get a connection,
+// -> run onConnection
+io.on("connection", onConnection);
+
 // Serves our front end
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/index.html"));
