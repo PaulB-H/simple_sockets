2119d7c932aea989865b49cc9a83f4bc3655ee1e
PaulB-H <p.bernardhall@gmail.com>
2021-02-26 15:01:06 -0500

    Removed extra io.on connection event
    Calling io.on connect inside this route handler was causing duplicate events / handlers to get attached for the same event.

--- socket_logic/socketConnect.js
+++ socket_logic/socketConnect.js
@@ -1,14 +1,3 @@
 module.exports = (io, socket) => {
-  // Listen for io.connection event
-  io.on("connection", () => {
-    console.log(`Socket: ${socket.id} connected`);
-  });
-
-  // Listen for socket disconnect event
-  socket.on("disconnecting", () => {
-    console.log(`Socket: ${socket.id} left`);
-  });
-
-  // We could also call functions / events like this
-  // io.on("connection", logSocket);
+  // Here we would do check for login token
 };
