fe2d96f40713f41d9661e8068ba55b2a0b3276a8
PaulB-H <p.bernardhall@gmail.com>
2021-02-26 16:07:56 -0500

    Setup client script to emit setUsername

--- client/script.js
+++ client/script.js
@@ -1,1 +1,10 @@
 const socket = io();
+
+// Name UI & Elements
+const setNameUI = document.getElementById("set-name-ui");
+const nameDiv = document.getElementById("name-input");
+
+const setUsername = () => {
+  const nameValue = nameDiv.value;
+  socket.emit("setUsername", nameValue);
+};
