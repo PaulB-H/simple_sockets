84f44730cb621d4609ba6b3f9fc9398ae5df3cc3
PaulB-H <p.bernardhall@gmail.com>
2021-02-26 22:18:42 -0500

    Updated logic for setUsername on server & client side

--- client/script.js
+++ client/script.js
@@ -8,3 +8,15 @@ const setUsername = () => {
   const nameValue = nameDiv.value;
   socket.emit("setUsername", nameValue);
 };
+
+socket.on("nameSet", (payload) => {
+  console.log(`Name set to ${payload}`);
+});
+
+socket.on("nameTaken", () => {
+  console.log("Name taken");
+});
+
+socket.on("alreadyName", () => {
+  console.log("That already is my name...");
+});
--- socket_logic/testHandler.js
+++ socket_logic/testHandler.js
@@ -1,15 +1,27 @@
 module.exports = (io, socket, users) => {
   socket.on("setUsername", (payload) => {
     console.log(
-      `Socket: ${socket.id}\nWants to set their name to: ${payload}\n`
+      `Socket: ${socket.id} \nwants to set their socket.username to: ${payload}`
     );
-    if (!users.has(payload)) {
+
+    if (socket.username === payload) {
+      console.log(`Their name is already ${payload}...\n`);
+
+      socket.emit("alreadyName");
+    } else if (!users.has(payload)) {
       socket.username = payload;
-      console.log("Name not taken, set name to: " + socket.username);
-      // Setup emit to client with name
+
+      users.add(payload);
+
+      console.log(
+        `${socket.id}'s request accepted \nSet socket.username to ${payload}\n`
+      );
+
+      socket.emit("nameSet", socket.username);
     } else {
-      console.log("Name taken");
-      // Setup emit to client with error
+      console.log(`${socket.id}'s request denied, name exists\n`);
+
+      socket.emit("nameTaken");
     }
   });
 };
