6d3843f011b6bdc7ea782fa894bc5db07a805482
PaulB-H <p.bernardhall@gmail.com>
2021-02-27 06:09:10 -0500

    Updated disconnect handler to clear username from users

--- socket_logic/disconnectHandler.js
+++ socket_logic/disconnectHandler.js
@@ -1,5 +1,10 @@
-module.exports = (io, socket) => {
+module.exports = (io, socket, users) => {
   socket.on("disconnecting", () => {
     console.log(`Socket: ${socket.id} left\n`);
+
+    if (socket.username !== undefined) {
+      console.log(`Username was ${socket.username}`);
+      users.delete(socket.username);
+    }
   });
 };
