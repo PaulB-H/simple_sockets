9d71c5a2133beab8c2fd3a1a60206e73450fab2c
PaulB-H <p.bernardhall@gmail.com>
2021-02-28 19:12:11 -0500

    roomHandler.js: Created .on(reqCreateRoom)
    Here I log the creation request details, deny if sent from socket without username, emit error if room already exists, and lastly will join the socket to the room and add the room to the server side rooms array, and emit a success event to socket

--- socket_logic/roomHandler.js
+++ socket_logic/roomHandler.js
@@ -43,4 +43,43 @@ Password: ${pass === null ? "None" : "Included"}
       socket.emit("room404");
     }
   });
+
+  socket.on("reqCreateRoom", (reqRoomNum, pass) => {
+    console.log(`
+Room Creation Request
+Socket ID: ${socket.id}
+Room #: ${reqRoomNum}
+Password: ${pass === null ? "None" : "Included"}
+    `);
+
+    if (socket.username === undefined) {
+      console.log("Socket has no .username \nRequest denied \n");
+      return;
+    }
+
+    // If there are rooms at all
+    if (rooms.length > 0) {
+      rooms.forEach((room) => {
+        // Check for matching room num
+        if (room.roomNum === reqRoomNum) {
+          console.log("Room exists\n");
+          socket.emit("roomAlreadyExists");
+          return;
+        }
+      });
+    }
+
+    // Join socket to room
+    socket.join(`${reqRoomNum}`);
+    console.log(socket.rooms);
+
+    // Add room to server rooms array
+    rooms.push({
+      roomNum: reqRoomNum,
+      pass: pass,
+    });
+
+    // Tell socket room was created
+    socket.emit("roomCreated", reqRoomNum);
+  });
 };
