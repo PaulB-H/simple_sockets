a8099328cdb2aac2a4acd5e68a80785ece2f6b6c
PaulB-H <p.bernardhall@gmail.com>
2021-03-03 06:49:42 -0500

    messageHandler.js: Adjusted when validator.escape() happens
    Had to make sure we are not trying to run this with the incorrect type of data or it will crash server

--- socket_logic/messageHandler.js
+++ socket_logic/messageHandler.js
@@ -3,14 +3,22 @@ const validator = require("validator");
 module.exports = (io, socket, users) => {
   // START sendMessage
   socket.on("sendMessage", (message) => {
+    if (message === "" || typeof message !== "string") {
+      socket.emit("noMessageFound");
+      return;
+    } else if (!message.trim()) {
+      socket.emit("noMessageFound");
+      return;
+    } else {
+      message = validator.escape(message);
+    }
+
     let currentUserObj;
     users.forEach((user) => {
       if (user.socketID === socket.id) {
         currentUserObj = user;
       }
     });
-
-    message = validator.escape(message);
 
     console.log(`Send Message Request
 Socket ID: ${currentUserObj.socketID}
