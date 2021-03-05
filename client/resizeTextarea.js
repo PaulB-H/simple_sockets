a2134f7b7202510c1e24e5ba34fdc34f0ebe6bfd
PaulB-H <p.bernardhall@gmail.com>
2021-03-05 01:26:50 -0500

    resizeTextarea.js: Specified overflowY to scroll, not both

--- client/resizeTextarea.js
+++ client/resizeTextarea.js
@@ -45,7 +45,7 @@ for (let i of textareas) {
       i.style.resize = "none";
 
       // This removes scrollbars
-      i.style.overflow = "scroll";
+      i.style.overflowY = "scroll";
 
       // Every input/change, grab the content
       content = i.value;
